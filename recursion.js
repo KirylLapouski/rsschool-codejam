f90bc38d8c6808b099d344ffcd21f42f8c9fa5ff
KirylLapouski <lapkovskyk@mail.ru>
2018-10-28 10:11:46 +0300

    fix: reverse ansver

--- recursion.js
+++ recursion.js
@@ -2,14 +2,16 @@ function recursion(...objs){
     // console.log(objs)
     if(objs.length === 0)
         return []
-    return recursion( ...objs.reduce((acc,obj)=>{ 
+    let res = recursion( ...objs.reduce((acc,obj)=>{
         if(obj.left)
         acc.push(obj.left)
         if(obj.right)
         acc.push(obj.right)
         return acc
-    }, [])).concat([objs.map(val=>val.value)])
-     
+    }, []))
+    res.unshift(objs.map(val=>val.value))
+    return res
+
 }
 
 console.log(recursion( {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}}))
